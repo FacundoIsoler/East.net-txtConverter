4c498e2c40b43b4d116246c46b9cd2bca7ef1b30
Facundo Isoler <facundoisoler@outlook.com>
2023-11-28 01:43:25 -0300

    clients

--- src/components/TangoSoftware/data/clients/Clients.jsx
+++ src/components/TangoSoftware/data/clients/Clients.jsx
@@ -1,19 +1,64 @@
-// En Clientes.jsx
-import React from 'react';
+// En Clients.jsx
+import React, { useState } from 'react';
+import './Clients.css';
 
 const Clients = () => {
-    // console.log(JSON.parse(localStorage.tangoData)[0], "clients");
-    // console.log(JSON.parse(localStorage.tangoData).length);
+    const [filterValueRS, setFilterValueRS] = useState('');
+    const [filterValueCC, setFilterValueCC] = useState('');
 
     if (!localStorage.tangoData || JSON.parse(localStorage.tangoData).length === 0) {
-        // Manejar el caso en que jsonData esté vacío o no sea un array
-        return <div>No hay datos de clientes disponibles.</div>;
+        return <div className="no-data-message">No hay datos de clientes disponibles.</div>;
     }
+
     const tangoData = JSON.parse(localStorage.tangoData);
 
+    const handleFilterChangeRS = (event) => {
+        setFilterValueRS(event.target.value);
+    };
+
+    const handleFilterChangeCC = (event) => {
+        setFilterValueCC(event.target.value);
+    };
+
+    const filteredClientsRS = tangoData.filter(
+        (cliente) =>
+            cliente.RAZON_SOCIAL.toLowerCase().includes(filterValueRS.toLowerCase()) 
+    );
+
+    const filteredClientsCC = tangoData.filter(
+        (cliente) =>
+            cliente.COD_CLIENTE.toString().includes(filterValueCC) || filterValueCC === ''
+    );
+
     return (
-        <div>
+        <div className="clients-container">
             <h1>Clientes</h1>
+            <div className="filter-section">
+                <label htmlFor="filterInputRS">Filtrar por Razón Social:</label>
+                <input
+                    type="text"
+                    id="filterInputRS"
+                    className="filter-input"
+                    value={filterValueRS}
+                    onChange={handleFilterChangeRS}
+                />
+                <button className="filter-button" onClick={() => setFilterValueRS('')}>
+                    Limpiar Filtro
+                </button>
+            </div>
+            <div className="filter-section">
+                <label htmlFor="filterInputCC">Filtrar por Código de Cliente:</label>
+                <input
+                    type="number"
+                    id="filterInputCC"
+                    className="filter-input"
+                    value={filterValueCC}
+                    onChange={handleFilterChangeCC}
+                />
+                <button className="filter-button" onClick={() => setFilterValueCC('')}>
+                    Limpiar Filtro
+                </button>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -24,9 +69,14 @@ const Clients = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {tangoData.map((cliente, index) => (
+                    {(filterValueRS || filterValueCC === ''
+                        ? tangoData
+                        : filterValueRS
+                        ? filteredClientsRS
+                        : filteredClientsCC
+                    ).map((cliente, index) => (
                         <tr key={index}>
-                            <td>{cliente.COD_CLIENTE}</td>
+                            <td>{parseInt(cliente.COD_CLIENTE)}</td>
                             <td>{cliente.RAZON_SOCIAL}</td>
                             <td>{cliente.TELEFONO}</td>
                             <td>{`${cliente.TIPO_DE_DOCUMENTO}: ${cliente.NUMERO}`}</td>
